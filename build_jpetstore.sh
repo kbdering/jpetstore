1f1b0fee2e3479838b116ffdf1ebbedc9d796a11
kuba <kb.dering@gmail.com>
2024-04-11 09:52:19 +0000

    automated launch

---
+++ build_jpetstore.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/mybatis/jpetstore-6
+cp pom.xml jpetstore-6/
+cp applicationContext.xml jpetstore-6/src/main/webapp/WEB-INF/
+docker build -t localhost:5000/my-jpetstore jpetstore-6/
+docker push localhost:5000/my-jpetstore 
---
+++ start_kube.sh
@@ -0,0 +1,1 @@
+minikube start --insecure-registry=192.168.49.1:5000
