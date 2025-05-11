466eea36a211d585782ec9552db74c9b2ed7ecc7
kbdering <kb.dering@gmail.com>
2025-05-11 11:53:07 +0200

    Moved from mvn cargo to tomcat native container

--- build_jpetstore.sh
+++ build_jpetstore.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/mybatis/jpetstore-6
 cp pom.xml jpetstore-6/
+cp Dockerfile jpetstore-6/
 cp applicationContext.xml jpetstore-6/src/main/webapp/WEB-INF/
-docker build -t localhost:5200/my-jpetstore jpetstore-6/
-docker push localhost:5200/my-jpetstore 
+docker build --platform linux/amd64 -t 192.168.1.122:5200/my-jpetstore jpetstore-6/
+docker push 192.168.1.122:5200/my-jpetstore 
